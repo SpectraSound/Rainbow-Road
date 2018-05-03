3241bce9ca864982bd9e6441b0c6855965ec2937
Kasper Hansen <kaha.skp@edu.mercantec.dk>
2018-05-03 12:54:18 +0200

    Player and Camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,33 +6,51 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody2D rb2D;
     public float JumpSpeed;
+    public float MoveSpeed;
     public bool IsGrounded = true;
+    public BoxCollider2D PlayerCollider2D;
+    public BoxCollider2D GroundCollider2D;
+    private Collision2D collision2D;
+    
+    
 
-	// Use this for initialization
-	void Start () {
-	    rb2D = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
+    // Use this for initialization
+    void Start() {
+        rb2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
 	void FixedUpdate () {
 	    PlayerJump();
+        PlayerMove();
 	}
 
     public void PlayerJump() {
 
-        if (IsGrounded) {
-
-            if (Input.GetKey(KeyCode.W)) {
-                rb2D.AddForce(new Vector2(0, JumpSpeed), ForceMode2D.Impulse);
-                IsGrounded = !IsGrounded;
-            }
-            
+        if (Input.GetKey(KeyCode.W) && IsGrounded)
+        {
+            rb2D.AddForce(new Vector2(0, JumpSpeed), ForceMode2D.Impulse);
+            IsGrounded = !IsGrounded;
         }
-        
-        
+
+        if (PlayerCollider2D.IsTouching(GroundCollider2D)) {
+            IsGrounded = true;
+        }
     }
 
     public void PlayerMove() {
+        if (Input.GetKey(KeyCode.D)) {
+            rb2D.AddForce(new Vector2(MoveSpeed,0));
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            rb2D.AddForce(new Vector2(-MoveSpeed, 0));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D Ground) {
         
     }
+
 }
